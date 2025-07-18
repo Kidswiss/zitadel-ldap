a875fd3dc5409fa04ee78954fe63d61e86597010
Simon Beck <simon.beck@vshn.ch>
2025-07-18 08:50:58 +0000

    Correct time comparison logic

--- zitadel/login.go
+++ zitadel/login.go
@@ -27,8 +27,8 @@ func (c *Client) Login(username, password string) (bool, error) {
 
 	// TODO: maybe we should check against the API if the session still exists...
 	if ct, ok := c.sessionCache.Load(hash); ok {
-		c.log.Debug().Str("username", username).Msg(fmt.Sprintf("Existing session available, %d seconds old", ct.Sub(time.Now()).Seconds()))
-		if ct.Sub(time.Now()).Seconds() > 3600 {
+		c.log.Debug().Str("username", username).Msg(fmt.Sprintf("Existing session available, %.2f seconds old", ct.Sub(time.Now()).Seconds()))
+		if time.Now().Sub(ct).Seconds() > 3600 {
 			c.log.Info().Str("username", username).Msg("Session expired, doing a new login")
 			return c.newSession(username, password)
 		}
